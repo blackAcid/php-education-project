7afa630ab02ee94763c4077a65ab3dd4e72a2807
yulia <yuliakostrikova@gmail.com>
2013-12-03 11:18:28 +0200

    PRS_2 applied for Core\View

--- application/core/View.php
+++ application/core/View.php
@@ -1,61 +1,66 @@
 <?php
+namespace Core\View;
+
 class View
 {
     private $path;
     private $page;
     private $include_file;
-    private $variables=array();
-    private $template='/template.php';
+    private $variables = array();
+    private $template = '/template.php';
     private $module;
 
     private $params = array(
         'xss_protection' => true
     );
 
-    public function __construct($module,$page)
+    public function __construct($module, $page)
     {
-        $this->path=DIR_MOD.$module."/views/";
-        $this->page=$page;
-        $this->module=$module;
+        $this->path = DIR_MOD . $module . "/views/";
+        $this->page = $page;
+        $this->module = $module;
     }
+
     public function addIntoTemplate()
     {
-        $this->include_file=$this->path.$this->page;
-        if(!file_exists($this->include_file)){
-            throw new Exception('Include file '.$this->include_file.' not exist');
+        $this->include_file = $this->path . $this->page;
+        if (!file_exists($this->include_file)) {
+            throw new Exception('Include file ' . $this->include_file . ' not exist');
         }
         return true;
     }
-    public function getCssFile(){
-        $links=array();
-        $path_to_css="css/".$this->module."/";
-        $matches = glob($path_to_css."*.css");
-        $s=substr($matches[0],strlen($path_to_css));
-        if ( is_array ( $matches ) ) {
-            for($i=0;$i<count($matches);$i++) {
-               $temp='<link rel="stylesheet" type="text/css" media="screen" href="'."/".$matches[$i].'" />';
-               $links[$i]=$temp;
-            }
-        }
-        return $links;
-    }
 
-    public function getJsFile(){
-        $links=array();
-        $path_to_js="js/".$this->module."/";
-        $matches = glob($path_to_js."*.js");
-        if ( is_array ( $matches ) ) {
-            for($i=0;$i<count($matches);$i++) {
-                $temp='<script type="text/javascript" src="'."/".$matches[$i].'"></script>';
-                $links[$i]=$temp;
-            }
-        }
-        return $links;
-    }
-
-    public function assign($name,$value)
+    public function getCssFile()
     {
-        $this->variables[$name]=$value;
+        $links = array();
+        $path_to_css = "css/" . $this->module . "/";
+        $matches = glob($path_to_css . "*.css");
+        if (is_array($matches)) {
+            for ($i = 0; $i < count($matches); $i++) {
+                $temp = '<link rel="stylesheet" type="text/css" media="screen" href="' . "/" . $matches[$i] . '" />';
+                $links[$i] = $temp;
+            }
+        }
+        return $links;
+    }
+
+    public function getJsFile()
+    {
+        $links = array();
+        $path_to_js = "js/" . $this->module . "/";
+        $matches = glob($path_to_js . "*.js");
+        if (is_array($matches)) {
+            for ($i = 0; $i < count($matches); $i++) {
+                $temp = '<script type="text/javascript" src="' . "/" . $matches[$i] . '"></script>';
+                $links[$i] = $temp;
+            }
+        }
+        return $links;
+    }
+
+    public function assign($name, $value)
+    {
+        $this->variables[$name] = $value;
     }
 
     public function __get($name)
@@ -63,20 +68,23 @@ class View
         if (isset($this->variables[$name])) {
             $variable = $this->variables[$name];
 
-            if ($this->params['xss_protection'])
+            if ($this->params['xss_protection']) {
                 $variable = $this->xssProtection($variable);
+            }
             return $variable;
         }
 
-        return NULL;
+        return null;
     }
 
     private function xssProtection($variable)
     {
         if (is_array($variable)) {
             $protected = array();
-            foreach ($variable as $key=>$value)
+            foreach ($variable as $key => $value) {
                 $protected[$key] = $this->xssProtection($value);
+            }
+
             return $protected;
         }
         return htmlspecialchars($variable);
@@ -84,6 +92,6 @@ class View
 
     public function display()
     {
-        require_once (DIR .$this->template);
+        require_once(DIR . $this->template);
     }
 }
