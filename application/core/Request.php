ca7725c6bcf7ea5b10f0bd994d2148dd33d49a2d
anna <black_acid@mail.ru>
2013-12-06 11:56:59 +0200

    new parse

--- application/core/Request.php
+++ application/core/Request.php
@@ -10,7 +10,11 @@ class Request
     // private $view;
     public function __construct()
     {
-        //$this->parseURI();
+        $this->parseURI();
+        //echo "controller=".$this->controller."<br>module=".$this->module."<br>action=".$this->action;
+        //echo "<br>dir=".DIR_PUBLIC;
+        //echo "<br>document_root=".$_SERVER['DOCUMENT_ROOT'];
+
     }
     public function getController()
     {
@@ -31,10 +35,15 @@ class Request
 
     private function parseURI()
     {
-        $routes = explode('/', $_SERVER['REQUEST_URI']);
-        //  print_r($routes);
+        $rep=str_replace($_SERVER['DOCUMENT_ROOT'],'',DIR_PUBLIC);
+        //echo "<br>rep=".$rep;
+        //$rout=str_replace($rep,'',$_SERVER['REQUEST_URI']);
+        //echo "rout=".$rout;
+        //$routes = explode('/', $_SERVER['REQUEST_URI']);
+        $routes = explode('/', str_replace($rep,'',$_SERVER['REQUEST_URI']));
+        //print_r($routes);
         echo $this->isCssFile();
-        if (!empty($routes[1]) && !empty($routes[2])) {
+        /*if (!empty($routes[1]) && !empty($routes[2])) {
             $this->module=$routes[1];
             $this->controller=$routes[2];
         }
@@ -51,6 +60,25 @@ class Request
                 }
             } else {
                 $this->action = $routes[3];
+            }
+        }*/
+       if (!empty($routes[0]) && !empty($routes[1])) {
+            $this->module=$routes[0];
+            $this->controller=$routes[1];
+        }
+        if (!empty($routes[2])) {
+            $temp=preg_split("/\?/", $routes[2]);
+            if (!empty($temp)) {
+                $this->action=array_shift($temp);
+                for ($j=0; $j<count($temp); $j++) {
+                    $key_val=explode('&', $temp[$j]);
+                    $eq=strpos($key_val[$j], '=');
+                    $key=substr($key_val[$j], 0, $eq);
+                    $value=substr($key_val[$j], $eq+1);
+                    $this->params[$key][$value];
+                }
+            } else {
+                $this->action = $routes[2];
             }
         }
     }
